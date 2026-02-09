0e7ef1d7b65eb97bd78a628670653af0c1f7589a
ludovic-omb <ludovic.omombo@gmail.com>
2026-02-09 13:23:49 +0100

    Refactor SQL query to select specific event fields

--- sql/sql/stg_events.sql
+++ sql/sql/stg_events.sql
@@ -1,5 +1,15 @@
 WITH source AS (
-    SELECT *
+    SELECT
+        event_timestamp,
+        DATE(event_timestamp) AS event_date,
+        LOWER(event_name) AS event_name,
+        user_id,
+        session_id,
+        page_url,
+        device,
+        source,
+        country,
+        COALESCE(event_value, 0) AS event_value
     FROM raw_events
 )
 
